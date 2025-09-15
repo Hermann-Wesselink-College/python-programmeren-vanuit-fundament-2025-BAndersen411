6ea7bb03756de745bad0208b22f7b25768befab7
BAndersen411 <36764@lln.hethwc.nl>
2025-09-15 12:30:44 +0000

    new file:   Me messing around/Guess the Number 	new file:   Me messing around/Me messing around

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -1,1 +1,22 @@
-# opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
+start = 'Hello world I am here to destroy you'
+print(start)
+name = 'Benn'
+print('My name is',name,)
+age = 16
+print('I am',age, 'years old')
+verzendkosten = 5
+print(verzendkosten)
+productcode = "A136i"
+print(productcode)
+# Ik ben de comentaar aan het testen
+'''
+Veel tekst blah
+Blah
+Blah
+'''
+cost = 889283.22
+print(cost)
+favColour = input('What is your favourite colour?')
+print('Your favourite colour is' ,favColour)
+
+
