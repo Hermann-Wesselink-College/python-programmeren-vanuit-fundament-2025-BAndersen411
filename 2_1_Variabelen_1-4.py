0381d6603f3887fb962c35229e15345ba007eff5
BAndersen411 <36764@lln.hethwc.nl>
2025-09-18 07:42:55 +0000

    modified:   2_1_Variabelen_1-4.py 	modified:   Me messing around/Guess the Number 	modified:   Me messing around/Me messing around

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -18,5 +18,33 @@ cost = 889283.22
 print(cost)
 favColour = input('What is your favourite colour?')
 print('Your favourite colour is' ,favColour)
+print('You live at:')
 
+PostalCode = '1982HZ'
+print(PostalCode.lower())
 
+PlaceOfLiving = 'Amstelveen'
+print(PlaceOfLiving.upper())
+
+verzendmethode = 'PostNL'
+betaalmethode = 'iDeal'
+print(verzendmethode.lower())
+print(betaalmethode.upper())
+
+postcode = input('What is your postcode')
+print(postcode.upper())
+
+plaats = "Gasselternijveenschemond"
+print(len(plaats))
+
+voornaam = input('Wat is je voornaam?')
+achternaam = input('Wat is je achternaam?')
+print('Halo', voornaam, achternaam)
+naam = input('Wat is jouw naam')
+straat = input('In welke straat woon je?')
+huisnummer = input('Wat is jouw huis nummer?')
+postcode = input('Wat is jouw postcode')
+woonplaats = input('Wat is jouw woonplaats')
+print(naam)
+print(straat, huisnummer)
+print(postcode.upper(), woonplaats.upper())
